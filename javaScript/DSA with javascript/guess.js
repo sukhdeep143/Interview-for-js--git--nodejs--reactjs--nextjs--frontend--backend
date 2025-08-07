67d5c7045057dde5c44fcdfde379ecb5d197e8e7
Sukhdeep Singh <sukhdeepsingh141414@gmail.com>
2025-08-07 16:28:52 +0530

    update

---
+++ javaScript/DSA with javascript/guess.js
@@ -0,0 +1,39 @@
+console.log("Hello 1");
+
+console.log("Hello 2")
+setTimeout(()=>{
+    console.log("Hello 3");
+
+},0)
+
+console.log("Hello 4")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const promiseOne =  Promise.resolve("Something will happen")
+// promiseOne.then((Something)=>{
+//     console.log(Something);
+    
+// })
